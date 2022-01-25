77f86745daae6c6cdcf7f92eff1005524c332311
David <davo.melkumyan40@gmail.com>
2022-01-25 06:46:45 +0100

    Added navbar wrap button with pretty animation X

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -1,26 +1,13 @@
 ﻿(() => {
-    const navs = document.querySelectorAll(".navlink");
-    const langElem = document.querySelector("#lng");
-
-    const setNavBarActiveToggle = function (navs) {
-        if (navs) {
-            for (let i = 0; i < navs.length; i++) {
-                navs[i].addEventListener("click", function () {
-                    navs.forEach(c => c.classList.remove("nav-active"));
-                    this.classList.add("nav-active");
-                });
-            }
-        }
-    }
-
-    const updateLanguageList = function (lngElem) {
-        if (lngElem) {
-            fetch(); // TODO finish
-        }
-    }
-
     window.onload = () => {
-        setNavBarActiveToggle(navs);
+        const navs = document.querySelectorAll(".navlink");
+        const langElem = document.querySelector("#lng");
+        const navBtn = document.querySelector(".nav-btn-wrap");
+        if (navBtn) {
+            navBtn.addEventListener("click", function() {
+                this.classList.toggle("nav-btn-wrap-active");
+            })
+        }
     }
 })();
 
