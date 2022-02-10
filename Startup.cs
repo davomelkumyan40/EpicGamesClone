982fe63bf415d3fdb628c17aecd22b612ee58d3d
David <davo.melkumyan40@gmail.com>
2022-02-10 13:22:14 +0100

    just commit

--- Startup.cs
+++ Startup.cs
@@ -29,7 +29,7 @@ namespace EpicGames
             }
             else
             {
-                app.UseExceptionHandler("/Home/Error");
+                app.UseExceptionHandler("/Site/Error");
                 //app.UseHsts();
             }
 
